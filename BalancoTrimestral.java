5886e4111d5b7fb28133afb055c3c7e273227c86
arthur868 <ar7981534@gmail.com>
2024-08-28 18:51:40 +0000

    add

---
+++ BalancoTrimestral.java
@@ -0,0 +1,10 @@
+class TesteIdade{
+    public static void main(String[] args) {
+   int gastoJaneiro = 15000;
+   int gastoFevereiro = 17000;
+   int gastoMarco = 23000;
+   
+   int gastoTrimestral = gastoJaneiro + gastoFevereiro + gastoMarco;
+   System.out.println(gastoTrimestral); 
+    }
+}
---
+++ TesteIdade.java
@@ -0,0 +1,18 @@
+class TesteIdade{
+            public static void main(String[] args) {
+    int idade;
+    idade = 18; 
+
+    int idadeproximoano = idade +1;
+       double resultado = idade /2;  
+       boolean maiordeIdade;
+       maiordeIdade = idade>= 18;
+      int i = 5;
+      int j = i;
+      i = i + 1;
+
+       System.out.println(i,j);
+}
+}
+
+
